9f6bb7eb8baf4c56dc18c43d5e39cce0cfd01e65
josmas <josmasflores@gmail.com>
2016-01-11 17:44:08 +0000

    adds libraries and first attempt to register the device

--- appinventor/components/src/com/google/appinventor/components/runtime/AerogearPush.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/AerogearPush.java
@@ -5,14 +5,15 @@
 
 package com.google.appinventor.components.runtime;
 
+import android.util.Log;
 import com.google.appinventor.components.annotations.*;
 import com.google.appinventor.components.common.ComponentCategory;
 import com.google.appinventor.components.common.PropertyTypeConstants;
 import com.google.appinventor.components.common.YaVersion;
-import com.google.appinventor.components.runtime.errors.YailRuntimeError;
 
-
-import android.view.View;
+import org.jboss.aerogear.android.unifiedpush.RegistrarManager;
+import org.jboss.aerogear.android.unifiedpush.gcm.AeroGearGCMPushJsonConfiguration;
+import org.jboss.aerogear.android.core.Callback;
 
 @DesignerComponent(version = YaVersion.AEROGEARPUSH_COMPONENT_VERSION,
     description = "A new component ",
@@ -20,7 +21,13 @@ import android.view.View;
     nonVisible = true,
     iconName = "images/aerogearPush.png")
 @SimpleObject
+@UsesPermissions(permissionNames = "android.permission.INTERNET, android.permission.GET_ACCOUNTS," +
+    "android.permission.WAKE_LOCK, com.google.android.c2dm.permission.RECEIVE")
+@UsesLibraries(libraries = "aerogear-android-push.jar, aerogear-android-core.jar, stripped-play-services.jar")
 public final class AerogearPush extends AndroidNonvisibleComponent {
+
+  private static final String PUSH_REGISTRAR_NAME = "myPushRegistrar";
+  private static final String TAG = "AerogearPush";
 
   //TODO (jos) properties copied from Twitter; change names and docs accordingly.
   private String consumerKey = "";
@@ -33,6 +40,30 @@ public final class AerogearPush extends AndroidNonvisibleComponent {
    */
   public AerogearPush(ComponentContainer container) {
     super(container.$form());
+
+    //TODO (jos) This component only works with API 16+ : make the appropriate checks.
+    RegistrarManager.config(PUSH_REGISTRAR_NAME, AeroGearGCMPushJsonConfiguration.class)
+        .loadConfigJson(container.$context()) //TODO (jos) right now it is not finding the json file in assets
+        .asRegistrar();
+
+    RegistrarManager.getRegistrar(PUSH_REGISTRAR_NAME).register(container.$context().getApplicationContext(),
+        new Callback<Void>() {
+      @Override
+      public void onSuccess(Void data) {
+        Log.d(TAG, "Registration Succeeded");
+        Log.d(TAG, "Registration Succeeded");
+        Log.d(TAG, "Registration Succeeded");
+//        Toast.makeText(getApplicationContext(), "Yay, Device registered", Toast.LENGTH_LONG).show();
+      }
+
+      @Override
+      public void onFailure(Exception e) {
+        Log.e(TAG, e.getMessage(), e);
+        Log.e(TAG, e.getMessage(), e);
+        Log.e(TAG, e.getMessage(), e);
+//        Toast.makeText(getApplicationContext(), "Ops, something is wrong :(", Toast.LENGTH_LONG).show();
+      }
+    });
   }
 
   /**
